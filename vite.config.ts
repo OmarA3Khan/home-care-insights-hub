9453da3d76c25d6f6f826c2e1a89f8f44ca47092
Omar Azam <omar.azam@mazataz.com>
2025-04-11 16:56:06 +0100

    gh pages changes

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig(({ mode }) => ({
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
+  base:"/nursing-home/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
