9aa714e2dd98fa5159e5eeec66a3f3e8aea4d3ca
Omar Azam <omar.azam@mazataz.com>
2025-04-16 15:38:37 +0100

    reversed gh pages changes

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,6 @@ export default defineConfig(({ mode }) => ({
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
-  base:"/nursing-home/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
